414893e95ecfe8814d095f5b2b59f5385773df24
elvisoft <elvisoftbeta@gmail.com>
2020-11-06 19:46:06 -0300

    120 mins

---
+++ app/src/main/java/com/elvisoft/listcontacchat/ui/login/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.elvisoft.listcontacchat.ui.login;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.elvisoft.listcontacchat.MainActivity;
+import com.elvisoft.listcontacchat.R;
+import com.elvisoft.listcontacchat.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        final EditText usernameEditText = findViewById(R.id.txtusername);
+        final EditText passwordEditText = findViewById(R.id.txtpassword);
+        final Button loginButton = findViewById(R.id.btnlogin);
+        final Button registerButton = findViewById(R.id.btnRegister);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                String email = usernameEditText.getText().toString();
+                String password= passwordEditText.getText().toString();
+                loginUser(email,password);
+                loadingProgressBar.setVisibility(View.GONE);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void loginUser(String email, String pass){
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                    finish();
+                }
+                else {
+
+                    Toast.makeText(LoginActivity.this,"El Usuario y la contraseña no estan registrados!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+}
